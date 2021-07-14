0d90b8611265566035d8c10287a053c05f185d4f
snxx-lppxx <snmgreenx@gmail.com>
2021-07-14 22:42:51 +0500

    commitbot: minor

--- commitbot.py
+++ commitbot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Add progressbar2 and also make the function more modular.
 Add tkinter gui as well and add date time change options.
@@ -5,16 +7,16 @@ Add descriptions on commandline and package the entire thing
 Lets see where we can take this.
 P.S. Made by Devansh. 
 """
-from time import sleep
+
 import os
 
-count=1
+count = 1
 
 def update():
     if 'a.txt' in os.listdir():
-        os.system('rename a.txt b.txt')  #Renames file
+        os.system('rename a.txt b.txt') # Renames a file
     elif 'b.txt' in os.listdir():
-        os.system('rename b.txt a.txt')  #Renames file
+        os.system('rename b.txt a.txt') # Renames a file
     else:
         listdir=os.listdir()
         for list in listdir:
@@ -23,11 +25,11 @@ def update():
         os.system('type nul > a.txt')
 
 
-def pull(repo,url):
-    if os.path.exists(repo)==False:
+def pull(repo, url):
+    if os.path.exists(repo) == False:
         os.system('git clone ' + url)
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, ''+repo)
+    filename = os.path.join(dirname, '' + repo)
     os.chdir(filename)
 
 def push():
@@ -35,17 +37,17 @@ def push():
     os.system('cd')
     os.system('git add . && git commit -m "commit ' + str(count) + '"')
     os.system('git push origin master')
-    count+=1
+    count += 1
 
 def main():
-    repo=input("Enter Git Repository name: ")
+    repo = input("Enter Git Repository name: ")
     url = input("Enter Git Repository URL: ")
-    comm=int(input("Enter Number Of Commits To Perform: "))
-    pull(repo,url)
-    for i in range(0,comm):
+    comm = int(input("Enter Number Of Commits To Perform: "))
+    pull(repo, url)
+    for i in range(0, comm):
         update()
         push()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
